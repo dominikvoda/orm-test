27e2dd3aec45a8db7255098365dbe1660aef45f2
wassy <d.voda94@gmail.com>
2015-03-17 23:21:18 +0100

    finish

--- app/presenters/ProjectPresenter.php
+++ app/presenters/ProjectPresenter.php
@@ -22,7 +22,6 @@ class ProjectPresenter extends BasePresenter {
 
     /** @var Orm @inject */
     public $orm;
-    private $projectId;
 
     public function createComponentProjectsGrid() {
         $grid = new Datagrid;
@@ -49,68 +48,6 @@ class ProjectPresenter extends BasePresenter {
 
     public function actionDetail($id) {
         $this->template->project = $this->orm->project->getById($id);
-        //$this->template->ftp = $this->project->getFtp($id);
-        //$this->template->database = $this->project->getDatabase($id);
-        //$this->template->contacts = $this->orm->;
-        //$this->template->webhosting = $this->project->getWebhosting($id);
-        //$this->template->tasks = $this->tasks->getTasks($id);
     }
 
-    /* public function actionTasks($id) {
-      $this->projectId = $id;
-      $this->template->project = $this->project->getProject($id);
-      $this->template->tasks = $this->tasks->getTasks($id);
-      }
-
-      public function actionEditTask($id) {
-      $task = $this->tasks->getTask($id);
-      $this->projectId = $task->project;
-      $this->template->project = $this->project->getProject($this->projectId);
-      $this->template->task = $task;
-      $this->template->subtasks = $this->tasks->getSubTasks($id);
-      if ($task->parent != 0) {
-      $this['editTask']->setDefaults($task);
-      }
-      }
-
-      public function actionInfo($id) {
-
-      }
-
-      public function actionContacts($id) {
-
-      }
-
-      public function actionFiles($id) {
-
-      }
-
-      public function actionWebhosting($id) {
-
-      }
-
-      public function actionFtp($id) {
-
-      }
-
-      public function actionDatabase($id) {
-
-      }
-
-      public function getSubTasks($parentTask) {
-      return $this->tasks->getSubTasks($parentTask);
-      }
-
-      public function countSubTasks($tasks) {
-      return $this->tasks->countTasks($tasks);
-      }
-
-      public function getMainTasks() {
-      $tasks = $this->tasks->getTasks($this->projectId);
-      $array[0] = 'Toto je primární úkol';
-      foreach ($tasks as $task) {
-      $array[$task->id] = $task->name;
-      }
-      return $array;
-      } */
 }
